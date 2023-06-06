f34e88d24ff94d03f0d7b6c90085cf815069814b
Juan Felipe Jimenez Lozada <jimenezlozadajuanfelipe@gmail.com>
2023-06-06 18:42:37 -0500

    Update final skills and funcion para los skills

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,7 +6,6 @@ navClose = document.getElementById('nav-close')
 
 /*======================== MENU HIDDEN ======================*/
 if (navToggle) {
-
     navToggle.addEventListener('click', () => {
         navMenu.classList.add('show-menu')
     })
@@ -14,7 +13,6 @@ if (navToggle) {
 
 /*======================== MENU SHOW ======================*/
 if (navClose) {
-
     navClose.addEventListener('click', () => {
         navMenu.classList.remove('show-menu')
     })
@@ -29,3 +27,24 @@ function linkAction() {
     navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*======================== ACCORDION SKILLS  ======================*/
+
+const skillsContent = document.getElementsByClassName('skills__content'),
+    skillsHeader = document.querySelectorAll('.skills__header')
+
+
+function toggleSkills() {
+    let itemClass = this.parentNode.className
+
+    for (i = 0; i < skillsContent.length; i++) {
+        skillsContent[i].className = 'skills__content skills__close'
+    }
+    if (itemClass === 'skills__content skills__close') {
+        this.parentNode.className = 'skills__content skills__open'
+    }
+
+}
+skillsHeader.forEach((el) => {
+    el.addEventListener('click', toggleSkills)
+});
